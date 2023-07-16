4cc048ee17f7623b6fb9132f8646e5263c53d63f
FELIX CHERUIYOT <felix@intasend.com>
2023-07-16 14:54:22 +0300

    Style dashboard

--- access/models.py
+++ access/models.py
@@ -29,6 +29,9 @@ class Access(models.Model):
     status = models.CharField(max_length=15, choices=STATUSES, default=PENDING)
     root_access = models.BooleanField(default=False)
 
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
+
     def __str__(self) -> str:
         return f"{self.status} {self.ssh_key} - {self.host}"
 
