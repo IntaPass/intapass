44d9381a48f17a55ecb93cc2bde9a2541c2bfaef
FELIX CHERUIYOT <felix@intasend.com>
2023-07-16 10:23:12 +0300

    Update host file and access files

--- worker/manage.py
+++ worker/manage.py
@@ -3,6 +3,30 @@ import json
 import subprocess
 
 class Worker():
+    def generate_host_file(self, hosts: list) -> str:
+        temp = {
+            "all": {
+                "hosts": {
+                    "146.190.49.200": None
+                },
+                "children": {
+                    "webservers": {
+                        "hosts": {
+                            "remote": {
+                                "ansible_host": "146.190.49.200",
+                                "ansible_port": 22,
+                                "ansible_user": "root"
+                            }
+                        }
+                    }
+                }
+            }}
+        
+        file_name = "host.json"
+        with open(file_name, "w+") as p:
+            p.write(json.dumps(temp))
+        return file_name
+        
     def give_access(self, keys: list, hosts: list) -> None:
         playbook = [{
             "name": "Test script",
@@ -21,6 +45,8 @@ class Worker():
         file_name = "playbook.json"
         with open(file_name, "w+") as p:
             p.write(json.dumps(playbook))
-        subprocess.call(["ansible-playbook", file_name, "-i", "hosts.yml"])
+        host_file = self.generate_host_file(hosts)
+        subprocess.call(["ansible-playbook", file_name, "-i", host_file])
         os.remove(file_name)
+        os.remove(host_file)
 
